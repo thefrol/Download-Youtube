c25bae957990bd86161d5d29d7af94ee6700f2b3
Дмитрий Фроленко <orangefrol@gmail.com>
2023-02-08 02:02:36 +0300

    first commit

---
+++ Download-Youtube.psm1
@@ -0,0 +1,21 @@
+function Remove-SpecialCharactersFromFiles {
+    Get-ChildItem -file | %{
+        $Name = $_.Name
+        $NewName = $Name -creplace "(?:\uD83C[\uDF00-\uDFFF])|(?:\uD83D[\uDC00-\uDDFF])" -Replace '\P{IsBasicLatin}' -replace "'",'' 
+        Rename-Item -Path $_ -NewName $NewName
+    }
+    
+}
+
+function Get-Youtube {
+    param(
+        [switch] $RemoveNonAscii
+    )
+    yt-dlp -f bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4] --remux mp4 $args
+    yt-dlp -f bv+ba/b --recode mp4 $args
+
+    if($RemoveNonAscii){
+        Remove-SpecialCharactersFromFiles
+    }
+    
+}
